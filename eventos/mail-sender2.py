7f76d835bde3827feccc712a3c565c5802b40e59
Goncalo Manuel Lopes de Matos <goncalo_slb_matos@hotmail.com>
2021-07-20 03:05:20 +0100

    'ds'

--- eventos/mail-sender2.py
+++ eventos/mail-sender2.py
@@ -9,7 +9,7 @@ email_user = 'goncalomatos007@gmail.com'
 email_password = 'SUpertramp007'
 
 
-
+'''
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 
         smtp.login(email_user, email_password)
@@ -21,8 +21,8 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         msg = f'Subject: {subject} \n\n {body}'
 
         smtp.sendmail(email_user, 'goncalomatos007@gmail.com', msg)
-    
-url = "https://www.randomlists.com/email-addresses"
+  '''  
+url = "https://quarteto-arabesco.herokuapp.com/admin/auth/user/"
 EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 session = HTMLSession()
 
